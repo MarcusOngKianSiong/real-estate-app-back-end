c5e60b91ee5a556a035b8ec9a0ff2a775f0582e7
Marcus <marcusongkiansiong@gmail.com>
2022-10-16 12:11:01 +0800

    testing out express server

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express")
 const app = express()
 
 app.get('/',(req,res)=>{
-    res.render('something')
+    res.send('SENDING SOMETHING TO THE FRONT END!!!')
 })
 
 app.listen(8080,()=>{
